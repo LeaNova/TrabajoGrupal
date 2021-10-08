73c2e9f11a320ede45d8568334e5663392bf1f1f
Santiago8333 <91435566+Santiago8333@users.noreply.github.com>
2021-10-08 19:26:59 -0300

    1

--- src/modelo/Materia.java
+++ src/modelo/Materia.java
@@ -4,5 +4,59 @@ package modelo;
  * @author Gomez Jon Darian, Guardia Lucero Santiago Agustín, Heredia Leandro
  */
 public class Materia {
+     private int idMateria;
+    private String nombre;
+    private int anio;
+    private boolean activo;
+
+    public Materia(int idMateria, String nombre, int anio, boolean activo) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+        this.activo = activo;
+    }
+
+    public Materia(int anio,String nombre,  boolean activo) {
+        this.anio = anio;
+        this.nombre = nombre;
+        this.activo = activo;
+    }
+
+    
+
+    public Materia() {
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
     
 }
