6a53d2c6115339995fd7b4f4827014020bca13a9
Jon Gomez <darociion@gmail.com>
2021-10-09 03:39:41 -0300

    Edito class Alumno y AlumnoData

--- src/control/AlumnoData.java
+++ src/control/AlumnoData.java
@@ -1,8 +1,128 @@
 package control;
 
+import java.sql.*;
+import java.util.List;
+import java.util.ArrayList;
+import modelo.*;
+
 /**
  * @author Gomez Jon Darian, Guardia Lucero Santiago Agustín, Heredia Leandro
  */
 public class AlumnoData {
     
+    private Connection conexion;
+
+    public AlumnoData(Conexion conexion) {
+        try {
+            this.conexion = conexion.getConexion();
+        } catch (SQLException ex) {
+            System.out.println("Error en la conexion");
+        }
+    }
+    
+    public void agregarAlumno(Alumno alumno){
+        String sql = "INSERT INTO alumno (legajo,nombre,apellido,fecha_nac,activo) VALUES (?,?,?,?,?)";
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setInt(1, alumno.getLegajo());
+            ps.setString(2, alumno.getNombre());
+            ps.setString(3, alumno.getApellido());
+            ps.setDate(4, Date.valueOf(alumno.getFecNac()));
+            ps.setBoolean(5, alumno.isActivo());
+            
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            
+            if(rs.next()){
+                alumno.setIdAlumno(rs.getInt(1));
+            }
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al agregar al alumno" + ex);
+        }
+    }
+    
+    public void actualizarAlumno(Alumno alumno){
+        String sql = "UPDATE alumno SET legajo = ?, nombre = ?, apellido = ?, fecha_nac = ?, activo = ? WHERE id_alumno = ?";
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            
+            ps.setInt(1, alumno.getLegajo());
+            ps.setString(2, alumno.getNombre());
+            ps.setString(3, alumno.getApellido());
+            ps.setDate(4, Date.valueOf(alumno.getFecNac()));
+            ps.setBoolean(5, alumno.isActivo());
+            ps.setInt(6, alumno.getIdAlumno());
+            
+            ps.executeUpdate();
+            ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al actualizar al alumno" + ex);
+        }
+    }
+    
+    public Alumno buscarAlumno(int idAlumno){
+        Alumno a = null;
+        
+        String sql = "SELECT * FROM alumno WHERE id_alumno = ?";
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            
+            ps.setInt(1, idAlumno);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()){
+                a.setIdAlumno(rs.getInt(1));
+                a.setLegajo(rs.getInt(2));
+                a.setNombre(rs.getString(3));
+                a.setApellido(rs.getString(4));
+                a.setFecNac(rs.getDate(5).toLocalDate());
+                a.setActivo(rs.getBoolean(6));
+            }
+            
+        ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al buscar al alumno");
+        }
+        
+        return a;
+    }
+    
+    public List<Alumno> obtenerAlumnos(){
+        List<Alumno> alumnos = new ArrayList<>();
+        Alumno a = null;
+        
+        String sql = "SELECT * FROM alumno";
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                a.setIdAlumno(rs.getInt(1));
+                a.setLegajo(rs.getInt(2));
+                a.setNombre(rs.getString(3));
+                a.setApellido(rs.getString(4));
+                a.setFecNac(rs.getDate(5).toLocalDate());
+                a.setActivo(rs.getBoolean(6));
+                
+                alumnos.add(a);
+            }
+            
+        ps.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al buscar al alumno");
+        }
+        
+        return alumnos;
+    }
 }
--- src/modelo/Alumno.java
+++ src/modelo/Alumno.java
@@ -1,8 +1,86 @@
 package modelo;
 
+import java.time.LocalDate;
+
 /**
  * @author Gomez Jon Darian, Guardia Lucero Santiago Agustín, Heredia Leandro
  */
 public class Alumno {
+    
+    private int idAlumno;
+    private int legajo;
+    private String nombre;
+    private String apellido;
+    private LocalDate fecNac;
+    private boolean activo;
+
+    public Alumno(){
+        
+    }
+    
+    public Alumno(int legajo, String nombre, String apellido, LocalDate fecNac, boolean activo) {
+        this.legajo = legajo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fecNac = fecNac;
+        this.activo = activo;
+    }
+
+    public Alumno(int idAlumno, int legajo, String nombre, String apellido, LocalDate fecNac, boolean activo) {
+        this.idAlumno = idAlumno;
+        this.legajo = legajo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fecNac = fecNac;
+        this.activo = activo;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public LocalDate getFecNac() {
+        return fecNac;
+    }
+
+    public void setFecNac(LocalDate fecNac) {
+        this.fecNac = fecNac;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
     
 }
